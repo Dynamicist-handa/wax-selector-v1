1a9d0ac6393d899e29ac64ce5d6cb2c445d866ab
Dynamicist-handa <r.handa@ecopals.de>
2025-04-09 11:32:23 +0200

    Update: changed the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,19 @@ import pandas as pd
 import pdfplumber
 import re
 
+st.set_page_config(page_title="Wax Additive Selection: Physics-informed Optimization!", layout="wide")
+st.title("Physics-informed Wax Additive Selection: Optimize Processing!")
+
 st.markdown(
     """
-    <h1 style='text-align: center; color: #1f4f5a; white-space: nowrap;'>
-        Physics-informed Wax Additive Selection: Optimize Processing!
-    </h1>
+    <div style='text-align: center;'>
+        <a href='https://www.ecopals.de/' target='_blank'>
+            <img src='https://raw.githubusercontent.com/Dynamicist-handa/wax-selector-v1/main/ecopals_logo.png' width='120'>
+        </a>
+    </div>
     """,
     unsafe_allow_html=True
 )
-
 #st.image("ecopals_logo.png", width=160)
 
 
